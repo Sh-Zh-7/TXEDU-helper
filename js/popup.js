4f083403aa6bf3ee4c1db1f35607d68872c09cdf
Sh-Zh-7 <2431297348@qq.com>
2020-04-03 16:37:57 +0800

    Add dynamic js

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,15 @@
-// 给各个元素绑定相应的事件
 (function(){
   // 全局变量
   var body = document.body;
 
+  // 函数区域
+  function createNode(htmlStr) {
+	  var div = document.createElement("div");
+    div.innerHTML = htmlStr;
+    return div.childNodes[0];
+  }
+
+  // 给各个元素绑定相应的事件
   var fileUploader = document.querySelector("#fileUploader");
   fileUploader.onchange = function() {
     $("#location").val($("#fileUploader").val());
@@ -20,29 +27,46 @@
 
   var compare = document.querySelector("#compare");
   compare.onclick = function() {
-    // 未在选课名单中的
+    body.appendChild(document.createElement("hr"));
+    var div_whole = document.createElement("div");
+    div_whole.style = "display: inline-block;";
+    // 左侧的文本框区域
     var div_left = document.createElement("div");
+    div_left.className = "div_of_list";
+    div_left.style = "float:left;";
+    var p_left =document.createElement("p");
+    p_left.className = "p_of_list";
+    p_left.innerText = "未在选课名单中的："
+    var text_left = document.createElement("textarea");
+    text_left.className = "textarea_of_list form-control";
+    text_left.readOnly = true;
+    div_left.appendChild(p_left);
+    div_left.appendChild(text_left);
+    // 右侧的文本框区域
+    var div_right = document.createElement("div");
+    div_right.className = "div_of_list";
+    div_right.style = "float:right;";
+    var p_right = document.createElement("p");
+    p_right.className = "p_of_list";
+    p_right.innerText = "未在上课名单中的: "
+    var text_right = document.createElement("textarea");
+    text_right.className = "textarea_of_list form-control";
+    text_right.readOnly = true;
+    div_right.appendChild(p_right);
+    div_right.appendChild(text_right);
+    // 添加进DOM
+    div_whole.appendChild(div_left);
+    div_whole.appendChild(div_right);
+    body.appendChild(div_whole);
 
-    var not_in_course_list_label = document.createElement("p");
-    not_in_course_list_label.innerText = "未在选课名单中的";
-    body.appendChild(not_in_course_list_label);
-
-    var not_in_course_list = document.createElement("input");
-    not_in_course_list.type = "text";
-    not_in_course_list.value = "沈之豪";
-
-    div_left.appendChild(not_in_course_list_label);
-    div_left.appendChild(not_in_course_list);
-    body.appendChild(not_in_course_list);
-
-    // 未在上课名单中的
-    var not_in_class_list_label = document.createElement("p");
-    not_in_class_list_label.innerText = "未在上课名单中的";
-    body.appendChild(not_in_class_list_label);
-
-    var not_in_class_list = document.createElement("input");
-    not_in_class_list.type = "text";
-    not_in_class_list.value = "shzh";
-    body.appendChild(not_in_class_list);
+    // 最后温馨提示的dom字符串
+    var notify = "<p class=\"notify\">\
+    <span style=\"font-weight: bold;\">\
+    温馨提示:\
+    </span>\
+    昵称和真名可能有对不上的情况，所以以上两个文本框中的内容请手动核对\
+    </p>";
+    var notify_element = createNode(notify);
+    body.appendChild(notify_element);
   }
 })()
